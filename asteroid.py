22bf9fa15953585a137211feff0045944fc75e9b
kennysc <kenny.stcyr@outlook.com>
2025-09-25 07:29:54 -0400

    added shot.kill() in the bullet colision logic so the bullet would disapear

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
 import pygame
+from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
        self.position += self.velocity * dt 
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            angle = random.uniform(20, 50)
+            velo_1 = self.velocity.rotate(angle)
+            velo_2 = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            aste_1 = Asteroid(self.position.x, self.position.y, new_radius)
+            aste_2 = Asteroid(self.position.x, self.position.y, new_radius)
+            aste_1.velocity = velo_1 * 1.2
+            aste_2.velocity = velo_2 * 1.2
+
--- main.py
+++ main.py
@@ -37,7 +37,8 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if shot.collides(asteroid):
-                    asteroid.kill()
+                    shot.kill()
+                    asteroid.split()
 
             if player.collides(asteroid):
                 print("Game Over!")
